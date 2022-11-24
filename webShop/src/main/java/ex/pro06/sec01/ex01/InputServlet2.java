0e4434504083ad0089f3e4d13f7a676fe66663b7
gon <gonsland99@gmail.com>
2022-11-24 11:00:28 +0900

    getParameterNames를 이용한 Enumeration으로 요청값 다 받기

---
+++ webShop/src/main/java/ex/pro06/sec01/ex01/InputServlet2.java
@@ -0,0 +1,27 @@
+package ex.pro06.sec01.ex01;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ex/pro06/input2")
+public class InputServlet2 extends HttpServlet{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException{
+		req.setCharacterEncoding("utf-8");
+		Enumeration enu = req.getParameterNames();
+		while(enu.hasMoreElements()) {
+			String name = (String)enu.nextElement();
+			String[] values = req.getParameterValues(name);
+			for(String v:values) {
+				System.out.println("name: "+name+", value: "+v);
+			}
+		}
+		
+	}
+}
