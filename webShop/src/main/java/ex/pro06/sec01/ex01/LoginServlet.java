547e86f0873d7eda6533a6a234754763eb0ead59
gon <gonsland99@gmail.com>
2022-11-24 10:59:07 +0900

    getParameter를 이용한 요청값 받기

---
+++ webShop/src/main/java/ex/pro06/sec01/ex01/LoginServlet.java
@@ -0,0 +1,22 @@
+package ex.pro06.sec01.ex01;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ex/pro06/login")
+public class LoginServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException{
+		req.setCharacterEncoding("utf-8");
+		String id = req.getParameter("id");
+		String pw = req.getParameter("pw");
+		
+		System.out.println(id);
+		System.out.println(pw);
+	}
+}
