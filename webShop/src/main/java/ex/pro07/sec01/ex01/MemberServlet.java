f03c216d7cf3256ae07a446460ca1e2edee8c465
gon <gonsland99@gmail.com>
2022-11-25 14:49:33 +0900

    Statement를 이용한 jdbc 연동

---
+++ webShop/src/main/java/ex/pro07/sec01/ex01/MemberServlet.java
@@ -0,0 +1,39 @@
+package ex.pro07.sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ex/pro07/member")
+public class MemberServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException{
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> list = dao.listMembers();
+		
+		out.print("<html><body>");
+		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+		
+		for(int i=0; i<list.size(); i++) {
+			MemberVO vo = list.get(i);
+			String id = vo.getId();
+			String pwd = vo.getPw();
+			String name = vo.getName();
+			String email = vo.getEmail();
+			Date joinDate = vo.getJoinDate();
+			out.print("<tr><td>"+id+"</td><td>"+pwd+"</td><td>"+name+"</td><td>"+email
+					+"</td><td>"+joinDate+"</td></tr>");
+		}
+		out.print("</table></body></html>");
+	}
+}
