738e2dcd743e41a852f9b1338f3920500c1066f9
gon <gonsland99@gmail.com>
2022-11-26 21:07:12 +0900

    redirect와 Dispatcher forward 차이

---
+++ webShop/src/main/java/ex/pro08/sec03/ex02/FirstServlet.java
@@ -0,0 +1,22 @@
+package ex.pro08.sec03.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/first")
+public class FirstServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException{
+		req.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
+		RequestDispatcher dispatch = req.getRequestDispatcher("second?name=son");
+		dispatch.forward(req, resp);
+	}
+}
