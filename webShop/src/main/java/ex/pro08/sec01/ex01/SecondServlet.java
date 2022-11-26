156b525124f4de50c758a2f66b124ab525911685
gon <gonsland99@gmail.com>
2022-11-26 21:05:56 +0900

    포워딩(redirect, refresh, location)

---
+++ webShop/src/main/java/ex/pro08/sec01/ex01/SecondServlet.java
@@ -0,0 +1,22 @@
+package ex.pro08.sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet("/second")
+public class SecondServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException{
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body>");
+		out.println("sendRedirect 실습");
+		out.println("</body></html>");
+	}
+}
