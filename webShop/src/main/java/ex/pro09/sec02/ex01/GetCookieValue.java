e603ec4f10d4aaca0d9fb639adcc29449c12fdbe
gon <gonsland99@gmail.com>
2022-11-27 15:18:32 +0900

    persistence, session 쿠키생성

---
+++ webShop/src/main/java/ex/pro09/sec02/ex01/GetCookieValue.java
@@ -0,0 +1,26 @@
+package ex.pro09.sec02.ex01;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/get")
+public class GetCookieValue extends HttpServlet{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException{
+		Cookie[] c = req.getCookies();
+		for(int i=0; i<c.length; i++) {
+			if(c[i].getName().equals("ctest")) {
+				System.out.println(URLDecoder.decode(c[i].getValue(),"utf-8"));
+			}
+		}
+	}
+}
---
+++ webShop/src/main/java/ex/pro09/sec02/ex01/SetCookieValue.java
@@ -0,0 +1,25 @@
+package ex.pro09.sec02.ex01;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/set")
+public class SetCookieValue extends HttpServlet{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException{
+		Date d = new Date();
+		Cookie c = new Cookie("ctest", URLEncoder.encode("쿠키 test", "utf-8"));
+//		c.setMaxAge(24*60*60);
+		c.setMaxAge(-1);
+		resp.addCookie(c);
+		System.out.println("현재시간: "+d);
+	}
+}
